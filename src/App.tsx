31baa1c11ba27fc5d7f64e9ea7da059bfe720310
vadim <test@test.com>
2021-11-05 17:19:51 +0200

    add custom eslint

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const blblb = ''
+
   return (
     <div className="App">
       <div className='flex justify-center'>Test</div>
---
+++ src/components/Cards/CardItem/index.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+export const CardItem = () => {
+  const temp = [];
+
+  return (
+    <div>
+
+    </div>
+  )
+}
